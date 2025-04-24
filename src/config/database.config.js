f833253530956cc558b4ef8cc9b9346a30f80c72
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-24 18:10:09 -0300

    refactor: update database configuration to use database.config.js and remove config.json

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,10 +2,23 @@ const { config } = require('dotenv')
 config()
 
 module.exports = {
-  dialect: process.env.DIALECT, //Qual banco de dados está utilizando;
-  host: process.env.HOST, //Qual servidor está utilizando;
-  username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
-  password: process.env.PASSWORDDB, //Qual a senha do seu usuário no postgres;
-  database: process.env.DATABASE, //Qual o nome do seu database no postgres;
-  port: process.env.PORT //Qual porta do seu postgres (Normalmente é a 5432);
+  dialect: process.env.DIALECT, 
+  host: process.env.HOST,
+  username: process.env.USERNAMEDB,
+  password: process.env.PASSWORDDB,
+  database: process.env.DATABASE,
+  port: process.env.PORT_DB,
+
+  dialectOptions: {
+    ssl: {
+    rejectUnauthorized: true,
+    require: true
+    }
+    },
+    pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+    }
 };
