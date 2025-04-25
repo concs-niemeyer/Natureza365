6415612a91cc23cb7536cfab2ab0caaf51466637
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-25 17:47:35 -0300

    Update .gitignore to include /.vscode and fix database port environment variable reference

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -7,7 +7,7 @@ module.exports = {
   username: process.env.USERNAMEDB, //Qual o nome do seu usuário no postgres;
   password: process.env.PASSWORDDB, //Qual a password do seu usuário no postgres;
   database: process.env.DATABASE, //Qual o nome do seu database no postgres;
-  port: process.env.PORT, //Qual porta do seu postgres (Normalmente é a 5432);
+  port: process.env.PORT_DB, //Qual porta do seu postgres (Normalmente é a 5432);
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
