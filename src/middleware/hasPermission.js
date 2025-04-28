7234d4d440deab033e8a3c44f96b5baf6d96681e
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-27 21:26:15 -0300

    Refactor hasPermission middleware for improved readability and consistency

--- src/middleware/hasPermission.js
+++ src/middleware/hasPermission.js
@@ -1,67 +1,67 @@
-const jwt = require('jsonwebtoken');
-const Permission = require('../models/Permission');
-const PermissionRole = require('../models/PermissionRole');
+const jwt = require("jsonwebtoken");
+const Permission = require("../models/Permission");
+const PermissionRole = require("../models/PermissionRole");
 
 function hasPermission(permissions) {
-    return async (req, res, next) => {
-        // Verifica se o cabeçalho de autorização está presente
-        if (!req.headers.authorization) {
-            return res.status(401).send({ message: "Token não fornecido" });
-        }
+  return async (req, res, next) => {
+    // Verifica se o cabeçalho de autorização está presente
+    if (!req.headers.authorization) {
+      return res.status(401).send({ message: "Token não fornecido" });
+    }
 
-        const token = req.headers.authorization // Extraia o token {Bearer <token>}
-        // Verifica se o token existe		
-        if (!token) {
-            return res.status(401).send({ message: "Token não fornecido" });
-        }
+    const token = req.headers.authorization; // Extraia o token {Bearer <token>}
+    // Verifica se o token existe
+    if (!token) {
+      return res.status(401).send({ message: "Token não fornecido" });
+    }
 
-        // Faz a desestruturação do token e verifica se o token é válido 
-        const decoded = jwt.verify(token, process.env.SECRET_JWT);
-        req.payload = decoded;
-        console.log(":::PAYLOAD:::");
-        console.log(decoded);
+    // Faz a desestruturação do token e verifica se o token é válido
+    const decoded = jwt.verify(token, process.env.SECRET_JWT);
+    req.payload = decoded;
+    console.log(":::PAYLOAD:::");
+    console.log(decoded);
 
-        try {
-            const roles = await PermissionRole.findAll({
-                where: {
-                    roleId: req.payload.roles.map((role) => role.id)
-                },
-                attributes: ['permissionId'],
-                include: [{ model: Permission, as: 'permissions' }]
-            });
+    try {
+      const roles = await PermissionRole.findAll({
+        where: {
+          roleId: req.payload.roles.map((role) => role.id),
+        },
+        attributes: ["permissionId"],
+        include: [{ model: Permission, as: "permissions" }],
+      });
 
-            console.log(":::ROLES :::");
-            console.log(roles);
+      console.log(":::ROLES :::");
+      console.log(roles);
 
-            // Mapeando os permissionIds e verificando se existe permissão
-            const permissions = roles.map(role => role.dataValues.permissions);
-            console.log("::PERMISSIONS:::")
-            console.log(permissions)
+      // Mapeando os permissionIds e verificando se existe permissão
+      const permissionIds = roles.map((role) => role.dataValues.permissionId);
+      console.log(permissionIds);
 
-            const existPermission = permissions.some((role) => {
-                return role.permissions.some((p) => {
-					
-					console.log(":::PERMISSÕES:::",p.description)
-                    return permissions.includes(p.description);
-                });
-            });
+      const existPermission = roles.some((role) => {
+        return role.permissions.some((p) => {
+          console.log(":::PERMISSÕES:::", p.description);
+          return permissions.includes(p.description);
+        });
+      });
 
-            console.log(":::EXISTE A PERMISSÃO:::");
-            console.log(existPermission);
+      console.log(":::EXISTE A PERMISSÃO:::");
+      console.log(existPermission);
 
-            if (!existPermission) {
-                return res.status(403).send({ message: "Você não tem autorização para este recurso." });
-            }
+      if (!existPermission) {
+        return res
+          .status(403)
+          .send({ message: "Você não tem autorização para este recurso." });
+      }
 
-            next();
-        } catch (error) {
-            console.log(error);
-            return res.status(401).send({
-                message: "Autenticação Falhou",
-                cause: error.message
-            });
-        }
-    };
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(401).send({
+        message: "Autenticação Falhou",
+        cause: error.message,
+      });
+    }
+  };
 }
 
 module.exports = { hasPermission };
